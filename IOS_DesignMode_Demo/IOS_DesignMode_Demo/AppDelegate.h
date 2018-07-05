bdd64655535c2b8be9a8ef00ca794fe39c3161e1
Riven <mac@MacdeiMac.local>
2018-07-05 15:05:59 +0800

    添加工程

---
+++ IOS_DesignMode_Demo/IOS_DesignMode_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_DesignMode_Demo
+//
+//  Created by Mac on 2018/7/5.
+//  Copyright © 2018年 Riven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS_DesignMode_Demo/IOS_DesignMode_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS_DesignMode_Demo
+//
+//  Created by Mac on 2018/7/5.
+//  Copyright © 2018年 Riven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
